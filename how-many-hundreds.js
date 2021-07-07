c000ff95a9defa64e3e274b2ec93b7361edb9950
Rheaazarraga <rheaazarraga@gmail.com>
2021-07-07 23:03:30 +0000

    completed function challenge

---
+++ how-many-hundreds.js
@@ -0,0 +1,13 @@
+function howManyHundreds (num) {
+  let count = 0;
+for (let i = num; i >= 100; i -= 100){
+count++;
+}
+return count;
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
