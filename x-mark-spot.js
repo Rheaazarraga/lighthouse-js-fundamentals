01b5767f009d22ed43a7ee0da5d77e04b31b1112
Rheaazarraga <rheaazarraga@gmail.com>
2021-07-07 03:23:25 +0000

    completed challenge with functions

---
+++ x-mark-spot.js
@@ -0,0 +1,23 @@
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north'];
+function finalPosition (moves) {
+  let currentSpot = [0,0]
+  for (let move of moves) {
+    switch(move)
+    {
+      case "north" :
+        ++currentSpot[1];
+        break;
+        case "south" :
+        --currentSpot[1];
+        break; 
+        case "east" :
+          ++currentSpot [0];
+          break;
+          case "west" :
+            --currentSpot[0];
+            break;
+    }
+  }
+  return currentSpot;
+}
+console.log(finalPosition(moves));
